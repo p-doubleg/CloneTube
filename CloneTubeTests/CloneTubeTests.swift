09d2514ff49fc3bc07eb6d7ab3ea1ac13229de10
George Partskhaladze <georgy.partskhaladze@granite.ie>
2025-08-27 21:11:19 +0400

    Initial Commit

---
+++ CloneTubeTests/CloneTubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloneTubeTests.swift
+//  CloneTubeTests
+//
+//  Created by George Partskhaladze on 27.08.25.
+//
+
+import Testing
+@testable import CloneTube
+
+struct CloneTubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
